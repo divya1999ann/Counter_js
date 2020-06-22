989cfab22c4267a7cd696a950d847f881a7b1a75
Divya Ann Kurien <divya.kurien@gmail.com>
2020-06-22 23:29:38 +0530

    Counter updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//let i=-1;
 
 function func(){
     if(document.getElementById("t1").value=="0"){
@@ -6,33 +5,6 @@ function func(){
     document.getElementById("t1").value = "25";
     }
 }
-
-/*function Updatecount(){
-        var c = document.getElementById("t1").value;
-        c = parseInt(c);
-        c = c-1;
-        if(c==-1){
-        alert("Count reached 0");
-        c=26;
-        }
-        document.getElementById("t1").value = c.toString();
-}
-
-function funcstop() {
-    clearInterval(ti);
-}
-
-function funcstart(){
-    if(i==-1){
-        i=i*-1;
-        var ti = setInterval(Updatecount,1000);
-    }
-    else{
-        funcstop();
-    }  
-}
-
-*/
 
 
 function funcstart(d){
@@ -42,11 +14,13 @@ function funcstart(d){
     } else {
         d.interval=setInterval(function(){
             var c = document.getElementById("t1").value;
-        c = parseInt(c);
-        c = c-1;
-        if(c==-1){
-        alert("Count reached 0");
-        c=26;
+            c = parseInt(c);
+            c = c-1;
+            if(c==-1){
+            alert("Count reached 0");
+            c=25;
+            clearInterval(d.interval);
+        d.interval=undefined;
         }
         document.getElementById("t1").value = c.toString();
         },500);
